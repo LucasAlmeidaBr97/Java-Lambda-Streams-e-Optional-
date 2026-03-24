2b1ff7eae725ddb480b1a1b8f05aec9c1ff196ff
Lucas Tadeu Almeida Martins de Oliveira <143283670+LucasAlmeidaBr97@users.noreply.github.com>
2026-03-24 20:54:01 -0300

    Exemplo de thread utilizando função lambda

---
+++ LambdaAula1/LambdaAula1/src/Java8LambdaActionListener.java
@@ -0,0 +1,10 @@
+import javax.swing.JButton;
+
+public class Java8LambdaActionListener {
+
+    // JAVA 8 FUNÇÕES LAMBDA
+
+    public static void main(String[] args) {
+        JButton jButton = new JButton();
+    }
+}
