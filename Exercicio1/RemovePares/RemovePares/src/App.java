3de7acc3759ba45029edecbf8dee5c70a1b8f2cd
Lucas Tadeu Almeida Martins de Oliveira <143283670+LucasAlmeidaBr97@users.noreply.github.com>
2026-03-30 20:29:06 -0300

    Nova funcionalidade ao projeto
    Foi adicionada uma funcionalidade que permite a filtragem de numeros maiores que 10, e os separa em uma outra lista

--- Exercicio1/RemovePares/RemovePares/src/App.java
+++ Exercicio1/RemovePares/RemovePares/src/App.java
@@ -1,15 +1,29 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        List<Integer> intList = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        List<Integer> intList = new ArrayList<>(List.of(9, 10, 14, 20, 60, 30, 3, 5, 1));
+
+        Predicate<Integer> maior = n -> n > 10 ;
+
+        List<Integer> filtrados = intList.stream().filter(maior).collect(Collectors.toList());
+
+    
 
         intList.removeIf(i -> i % 2 == 0 );
         System.out.println(intList);
 
+        intList.stream();
+        
+
         intList.forEach(System.out::println);
+
+        filtrados.forEach(System.out::println);
+
+    
     }
 }
